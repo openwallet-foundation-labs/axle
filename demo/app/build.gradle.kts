3c78488daccfa54de2ce673eb2feb0c0246141ac
Lukas.J.Han <lukas.j.han@gmail.com>
2026-07-15 12:10:09 +0900

    chore(demo): bump version to 0.4 (versionCode 4)

--- demo/app/build.gradle.kts
+++ demo/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.hopae.axle.wallet"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
     }
 
     buildFeatures { compose = true }

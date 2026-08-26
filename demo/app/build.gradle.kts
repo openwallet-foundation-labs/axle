6c3b17edaadac310c39206a7dc6658a25cb5ca60
Lukas.J.Han <lukas.j.han@gmail.com>
2026-08-26 10:08:18 +0900

    chore: v10 (#82)
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- demo/app/build.gradle.kts
+++ demo/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.hopae.axle.wallet"
         minSdk = 29
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.9"
+        versionCode = 10
+        versionName = "0.10"
     }
 
     buildFeatures { compose = true }

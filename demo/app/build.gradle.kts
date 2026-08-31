f7b8054171bb49f3c27589d0b19b03285ed1dcc1
Lukas.J.Han <lukas.j.han@gmail.com>
2026-08-31 04:16:45 +0200

    chore: v12 (#89)
    Signed-off-by: Lukas J Han <lukas.j.han@gmail.com>

--- demo/app/build.gradle.kts
+++ demo/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.hopae.axle.wallet"
         minSdk = 29
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.11"
+        versionCode = 12
+        versionName = "0.12"
     }
 
     buildFeatures { compose = true }
